eb246ed514172ef0906dd649d7f879faa32ed1f7
Timothée Barray <tim@amicalement-web.net>
2016-01-14 15:45:59 +0100

    Fit to commandBus v2

--- Provider/SncRedis/SncRedisBus.php
+++ Provider/SncRedis/SncRedisBus.php
@@ -2,12 +2,9 @@
 
 namespace Rezzza\CommandBusBundle\Provider\SncRedis;
 
-use Psr\Log\LoggerInterface;
 use Rezzza\CommandBus\Domain\Serializer\CommandSerializerInterface;
 use Rezzza\CommandBus\Infra\Provider\Redis\RedisBus;
 use Rezzza\CommandBus\Infra\Provider\Redis\RedisKeyGeneratorInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
 
 /**
  * SncRedisBus
@@ -21,16 +18,11 @@ class SncRedisBus extends RedisBus
      * @param object                     $client          client
      * @param RedisKeyGeneratorInterface $keyGenerator    keyGenerator
      * @param CommandSerializerInterface $serializer      serializer
-     * @param EventDispatcherInterface   $eventDispatcher eventDispatcher
-     * @param LoggerInterface            $logger          logger
      */
-    public function __construct($client, RedisKeyGeneratorInterface $keyGenerator, CommandSerializerInterface $serializer, EventDispatcherInterface $eventDispatcher, LoggerInterface $logger = null)
+    public function __construct($client, RedisKeyGeneratorInterface $keyGenerator, CommandSerializerInterface $serializer)
     {
         $this->client          = $client;
         $this->keyGenerator    = $keyGenerator;
         $this->serializer      = $serializer;
-        $this->eventDispatcher = $eventDispatcher;
-        $this->logger          = $logger;
     }
-
 }
